2b6997e951420ffdf924f5523a55cb63b249b67b
GTK-kala <gtkkala1110@gmail.com>
2025-04-25 16:46:55 +0300

    seventh commit

--- React-Quiz-App/src/Components/Quiz/Quiz.jsx
+++ React-Quiz-App/src/Components/Quiz/Quiz.jsx
@@ -1,20 +1,50 @@
-import { useState } from 'react'
+import { useState , useRef } from 'react'
 import data from '../../../src/Assets/data.js'
 import './Quiz.css'
 
 const Quiz = () => {
-   let [index,setIndex] = useState(23);
+   let [index,setIndex] = useState(0);
 
-   let [questions,setQuestion] = useState(data[index])
-    
+   let [questions,setQuestion] = useState(data[index]);
+
+   let[lock,setLock] = useState(false); 
+
+   let[score,setScore] = useState(0);
+
+   let Option1 = useRef(null);
+   let Option2 = useRef(null);
+   let Option3 = useRef(null);
+   let Option4 = useRef(null); 
+
+   let Option_array = [Option1,Option2,Option3,Option4]
+
   const checkAns = (e,ans) =>{
-        if(questions.ans === ans){
-          e.target.classList.add("correct");
-        }
+    if(lock === false){
+       if(questions.answer === ans){
+        e.target.classList.add("correct");
+        setLock(true);
+        setScore((prev) => prev+1);
+       }
         else{
-          e.target.classList.add("wrong")
-        }
-  }   
+        e.target.classList.add("wrong");
+        setLock(true);
+        Option_array[questions.answer-1].current.classList.add("correct")
+      }
+    }
+  } 
+  
+  const Next = () =>{
+     if(lock === true){
+       setIndex(++index);
+       setQuestion(index);
+       setLock(false);
+       Option_array.map((option) =>{
+           option.current.classList.remove("correct");
+           option.current.classList.remove("wrong");
+           return null;
+       })
+     }
+  }
   return (
     <>
       <div className="container">
@@ -24,24 +54,24 @@ const Quiz = () => {
            {questions.question}
          </h2>
          <ul>
-          <li onClick={(e) =>{
+          <li ref={Option1} onClick={(e) =>{
             checkAns(e,1)
           }}>{questions.option1}</li>
-          <li onClick={(e) =>{
+          <li ref={Option2} onClick={(e) =>{
             checkAns(e,2)
           }}>{questions.option2}</li>
-          <li onClick={(e) =>{
+          <li ref={Option3} onClick={(e) =>{
             checkAns(e,3)
           }}>{questions.option3}</li>
-          <li onClick={(e) =>{
+          <li ref={Option4} onClick={(e) =>{
             checkAns(e,4)
           }}>{questions.option4}</li>
          </ul>
-          <button className="btu">
+          <button className="btu" onClick={Next}>
             Next
           </button>
           <div className="index">
-            1 of 5 qustions
+            {score} of {data.length} qustions
           </div>
       </div>
     </>
