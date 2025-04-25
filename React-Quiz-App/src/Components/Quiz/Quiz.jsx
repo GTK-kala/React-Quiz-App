4faff21789095ff1bc8d00d03cd4b74cdc3ed922
GTK-kala <gtkkala1110@gmail.com>
2025-04-25 11:48:53 +0300

    sixth commit

--- React-Quiz-App/src/Components/Quiz/Quiz.jsx
+++ React-Quiz-App/src/Components/Quiz/Quiz.jsx
@@ -1,21 +1,42 @@
-
+import { useState } from 'react'
+import data from '../../../src/Assets/data.js'
 import './Quiz.css'
 
 const Quiz = () => {
+   let [index,setIndex] = useState(23);
+
+   let [questions,setQuestion] = useState(data[index])
+    
+  const checkAns = (e,ans) =>{
+        if(questions.ans === ans){
+          e.target.classList.add("correct");
+        }
+        else{
+          e.target.classList.add("wrong")
+        }
+  }   
   return (
     <>
       <div className="container">
          <h1>Quiz App</h1>
          <hr />
          <h2>
-         Which device is required for the Internet connection?
+           {questions.question}
          </h2>
-          <ul>
-            <li>Modem</li>
-            <li>Router</li>
-            <li>LAN Cable</li>
-            <li>Africa</li>
-          </ul>
+         <ul>
+          <li onClick={(e) =>{
+            checkAns(e,1)
+          }}>{questions.option1}</li>
+          <li onClick={(e) =>{
+            checkAns(e,2)
+          }}>{questions.option2}</li>
+          <li onClick={(e) =>{
+            checkAns(e,3)
+          }}>{questions.option3}</li>
+          <li onClick={(e) =>{
+            checkAns(e,4)
+          }}>{questions.option4}</li>
+         </ul>
           <button className="btu">
             Next
           </button>
